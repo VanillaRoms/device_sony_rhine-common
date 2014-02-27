CyanogenMod/android_device_sony_rhine-common

Support C6802 and C6902 with a post-install script
* Modem firmware differs on C6[89]02.

Change-Id: I3569e37bdd01511b7122620486a954487db5412f

rhine.mk
@@ -171,6 +171,9 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/sbin/wait4tad_static:root/sbin/wait4tad_static \
     $(COMMON_PATH)/rootdir/sbin/tad_static:root/sbin/tad_static
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/c6x02.sh:c6x02.sh
+
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp

VanillaRoms/device_sony_rhine-common

Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

rhine.mk
@@ -174,6 +174,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XperiaSettings
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/install/c6x02.sh:c6x02.sh
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \